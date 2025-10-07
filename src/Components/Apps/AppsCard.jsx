0b002775c6eb5bb5e11fbc6cdd621afcb3d011d6
Skhsouravhalder <skhsouravhalder@gmail.com>
2025-10-08 01:27:04 +0600

    update home page

--- src/Components/Apps/AppsCard.jsx
+++ src/Components/Apps/AppsCard.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 const AppsCard = ({ singleDta }) => {
   console.log(singleDta);
-  return <div>card</div>;
+
+  const { image, companyName } = singleDta;
+
+  return (
+    <div className="   mt-10">
+      <div className="card bg-base-100  shadow-sm">
+        <figure className="h-48 overflow-hidden">
+          <img className="w-full object-cover" src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{companyName}</h2>
+          <p>
+            A card component has a figure, a body part, and inside body there
+            are title and actions parts
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AppsCard;
--- src/Components/Header/AppCard.jsx
+++ src/Components/Header/AppCard.jsx
@@ -3,17 +3,20 @@ import AppsCard from "../Apps/AppsCard";
 
 const AppCard = ({ data }) => {
   console.log(data);
+  const datas = data.slice(0, 8);
   return (
-    <div>
+    <div className="pb-[168px]">
       <h1 className="text-center font-bold text-[48px] mt-20 ">
         Trending Apps
       </h1>
       <p className="text-center text-gray-400">
         Explore All Trending Apps on the Market developed by us
       </p>
-      {data.map((singleDta) => (
-        <AppsCard singleDta={singleDta}></AppsCard>
-      ))}
+      <div className="px-[80px] grid grid-cols-4 gap-2">
+        {datas.map((singleDta) => (
+          <AppsCard singleDta={singleDta}></AppsCard>
+        ))}
+      </div>
     </div>
   );
 };
